6808320389b04056a708b7dfad16b5f2affcfc51
Ogunwole Isaac <tobiogunwole002@gmail.com>
2026-09-04 21:43:02 +0100

    Finalize UI enhancements and template views

--- core/settings.py
+++ core/settings.py
@@ -85,3 +85,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Auth redirects
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Email Backend (prints emails with reset tokens to console in dev)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
