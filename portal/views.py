08a6f4ce4325a5b11e8b5f180b3b17992762cfa2
Ogunwole Isaac <tobiogunwole002@gmail.com>
2026-09-05 01:13:17 +0100

    Force-reset admin user credentials and staff flag

--- portal/views.py
+++ portal/views.py
@@ -69,3 +69,12 @@ def signup(request):
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
+
+def create_admin_once(request):
+    user, created = User.objects.get_or_create(username='admin')
+    user.set_password('AdminPass123!')
+    user.is_staff = True
+    user.is_superuser = True
+    user.is_active = True
+    user.save()
+    return HttpResponse("Admin account successfully reset: username: admin | password: AdminPass123!")
